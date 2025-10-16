6214f63abd63895447c05ac585b31e4ecec56f74
siddhikharde <siddhikharde2005@gmail.com>
2025-10-17 00:35:18 +0530

    media query added

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
 import './main.css'
 import himalaya from './himalaya.jpg';
+import sahyadri from './sahyadri.jpg';
+import arvallis from './aravalli-range.jpg';
 import { createRoot } from 'react-dom/client'
 function Card(props){
 const {image, heading , subheading} = props;
 return(
   
-  <div className="card">
+ 
+   <div className="card">
     <img src={image} alt="image" className="card-image"/>
     <div className="container">
       <h4 style={{textAlign:"center", color:"black"}}><b>{heading}</b></h4>
       <p>{subheading}</p>
     </div>
   </div>
+
   
 )
 
@@ -20,11 +24,22 @@ return(
 
 createRoot(document.getElementById('root')).render(
   <>
-  <Card 
+  <div className="card-container">
+    <Card 
 image={himalaya}
 heading={"Himalaya"}
 subheading={"The Himalayas, a majestic mountain range in Asia, stretch across five countries: Bhutan, India, Nepal, China, and Pakistan. Home to the world's highest peaks, including Mount Everest, the Himalayas are renowned for their breathtaking landscapes, diverse ecosystems, and rich cultural heritage. This region is a hub for adventure tourism, attracting trekkers and mountaineers from around the globe. The Himalayas also hold significant spiritual importance, with numerous sacred sites and pilgrimage routes for various religions."}
 />
-
+  <Card 
+image={sahyadri}
+heading={"Sahyadri"}
+subheading={"The Sahyadri mountain range, also known as the Western Ghats, is a UNESCO World Heritage Site located in western India. Stretching along the western coast, it spans several states including Maharashtra, Karnataka, Kerala, and Tamil Nadu. The Sahyadri range is renowned for its rich biodiversity, hosting a variety of flora and fauna, many of which are endemic to the region. It is also known for its scenic beauty, with lush green landscapes, waterfalls, and hill stations like Munnar and Coorg. "}
+/>
+<Card 
+image={arvallis}
+heading={"Aravalli Range"}
+subheading={"The Aravalli Range is one of the oldest mountain ranges in India, stretching across the states of Rajasthan, Haryana, and Gujarat. It extends approximately 692 kilometers from Delhi in the north to Gujarat in the southwest. The range is characterized by its rugged terrain, rocky outcrops, and sparse vegetation. The Aravalli Range plays a crucial role in influencing the climate of the region, acting as a barrier to the monsoon winds. It is also rich in mineral resources, including deposits of copper, lead, zinc, and marble. "}
+/>
+  </div>
   </>
 )
